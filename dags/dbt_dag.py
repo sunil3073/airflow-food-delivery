992170af2192e4b3d0422cb2bd4118e45fd1468e
sunil3073 <sunilsirpi2018@gmail.com>
2026-05-02 12:45:10 +0530

    updated dag

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -1,9 +1,10 @@
 from airflow import DAG
-from airflow.providers.bash.operators import BashOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 REPO_URL = "https://github.com/sunil3073/food_project_dbt.git"
 DBT_DIR = "/tmp/dbt_project"
+PROFILES_DIR = "/usr/local/airflow/include"
 
 with DAG(
     dag_id="dbt_pipeline_dynamic",
@@ -25,32 +26,71 @@ with DAG(
         task_id="dbt_deps",
         bash_command=f"""
         cd {DBT_DIR} &&
-        dbt deps --profiles-dir /usr/local/airflow/include
+        dbt deps
         """
     )
 
+    # ---------- DEV ----------
     dbt_snapshot = BashOperator(
         task_id="dbt_snapshot",
         bash_command=f"""
         cd {DBT_DIR} &&
-        dbt snapshot --target dev --profiles-dir /usr/local/airflow/include
+        dbt snapshot --target dev --profiles-dir {PROFILES_DIR}
         """
     )
 
-    dbt_run = BashOperator(
-        task_id="dbt_run",
+    dbt_run_dev = BashOperator(
+        task_id="dbt_run_dev",
         bash_command=f"""
         cd {DBT_DIR} &&
-        dbt run --target dev --profiles-dir /usr/local/airflow/include
+        dbt run --target dev --profiles-dir {PROFILES_DIR}
         """
     )
 
-    dbt_test = BashOperator(
-        task_id="dbt_test",
+    dbt_test_dev = BashOperator(
+        task_id="dbt_test_dev",
         bash_command=f"""
         cd {DBT_DIR} &&
-        dbt test --target dev --profiles-dir /usr/local/airflow/include
+        dbt test --target dev --profiles-dir {PROFILES_DIR}
         """
     )
 
-    clone_repo >> dbt_deps >> dbt_snapshot >> dbt_run >> dbt_test
+    # ---------- QA ----------
+    dbt_run_qa = BashOperator(
+        task_id="dbt_run_qa",
+        bash_command=f"""
+        cd {DBT_DIR} &&
+        dbt run --target qa --profiles-dir {PROFILES_DIR}
+        """
+    )
+
+    dbt_test_qa = BashOperator(
+        task_id="dbt_test_qa",
+        bash_command=f"""
+        cd {DBT_DIR} &&
+        dbt test --target qa --profiles-dir {PROFILES_DIR}
+        """
+    )
+
+    # ---------- PROD ----------
+    dbt_run_prod = BashOperator(
+        task_id="dbt_run_prod",
+        bash_command=f"""
+        cd {DBT_DIR} &&
+        dbt run --target prod --profiles-dir {PROFILES_DIR}
+        """
+    )
+
+    dbt_test_prod = BashOperator(
+        task_id="dbt_test_prod",
+        bash_command=f"""
+        cd {DBT_DIR} &&
+        dbt test --target prod --profiles-dir {PROFILES_DIR}
+        """
+    )
+
+    # ---------- FLOW ----------
+    clone_repo >> dbt_deps >> dbt_snapshot \
+        >> dbt_run_dev >> dbt_test_dev \
+        >> dbt_run_qa >> dbt_test_qa \
+        >> dbt_run_prod >> dbt_test_prod
